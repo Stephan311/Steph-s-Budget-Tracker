025ff4fe917bdf509342bbee37f839c17bfe26e0
stephan du Plooy <sdpphotography1@gmail.com>
2021-07-10 16:49:47 -0400

    Added checkdatabase function to check if DB is offline

--- public/db.js
+++ public/db.js
@@ -20,7 +20,45 @@ if (database.objectNames.length === 0) {
 
 };
 
+request.onerror = function (e) {
+    console.log(`error ${e.target.errorCode}`)
+}
+
+function checkDatabase() {
+    console.log('check database was started')
+
+    let purchase = database.purchase(['BudgetDB'], 'readwrite');
+    const storeData = purchase.objectStore('BudgetDB')
+    const find = storeData.getAll();
+
+    find.onsuccess = function () {
+        if(find.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(find.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*,',
+                    'Content-Type': 'application/json',
+                },
+            }).then((result) => result.json())
+            .then((res) => {
+                if(res.length !== 0) {
+                    purchase = database.purchase(['BudgetDB'], 'readwrite');
+                }
+            });
+        }
+    }
+}
+
+
+
+
 const saveRecord = (record) => {
     console.log('Record has been saved');
     const purchase = db.purchase(['BudgetDB'], 'readrite');
-}
+
+const storeData = purchase.objectStore('BudgetDB');
+
+storeData.add(record);
+
+};
