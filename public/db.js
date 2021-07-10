f9b5214683fce46aee54516435b1156f3e48de94
stephan du Plooy <sdpphotography1@gmail.com>
2021-07-10 17:57:38 -0400

    Fixed issues with DB not exisiting

--- public/db.js
+++ public/db.js
@@ -1,23 +1,21 @@
-
 let database;
-let budgetTracker
+let budgetVersion
 
-const request = indexedDB.open("ExpenseDB", budgetTracker || 21);
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
 
 request.onupgradeneeded = function (e) {
-    console.log('Upgrade needed in IndexDB')
+  console.log('Upgrade needed in IndexDB');
 
-const { previousVersion } = e;
-const newerVersion = e.newerVersion || database.version;
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || database.version;
 
-console.log(`Databse has been updated from ${previousVersion} to ${newerVersion}`);
+    console.log(`Databse has been updated from ${oldVersion} to ${newVersion}`);
 
-db = e.target.result;
+    database = e.target.result;
 
-if (database.objectNames.length === 0) {
-    database.createObjectStore('BudgetDB', {autoincrement: true});
-}
-
+    if (database.objectStorenames.length === 0) {
+        database.createObjectStore('BudgetDB', { autoincrement: true });
+    }
 };
 
 request.onerror = function (e) {
@@ -27,12 +25,12 @@ request.onerror = function (e) {
 function checkDatabase() {
     console.log('check database was started')
 
-    let purchase = database.purchase(['BudgetDB'], 'readwrite');
-    const storeData = purchase.objectStore('BudgetDB')
+    let transaction = database.transaction(['BudgetStore'], 'readwrite');
+    const storeData = transaction.objectStore('BudgetStore')
     const find = storeData.getAll();
 
     find.onsuccess = function () {
-        if(find.result.length > 0) {
+        if (find.result.length > 0) {
             fetch('/api/transaction/bulk', {
                 method: 'POST',
                 body: JSON.stringify(find.result),
@@ -41,15 +39,15 @@ function checkDatabase() {
                     'Content-Type': 'application/json',
                 },
             }).then((result) => result.json())
-            .then((res) => {
-                if(res.length !== 0) {
-                    purchase = database.purchase(['BudgetDB'], 'readwrite');
+                .then((res) => {
+                    if (res.length !== 0) {
+                        transaction = database.transaction(['BudgetDB'], 'readwrite');
 
-                    const presentData = purchase.objectStore('BudgetDB');
-                    presentData.clear();
-                    console.log('clearing data')
-                }
-            });
+                        const presentData = transaction.objectStore('BudgetDB');
+                        presentData.clear();
+                        console.log('clearing data')
+                    }
+                });
         }
     }
 }
@@ -58,19 +56,20 @@ request.onsuccess = function (e) {
     console.log('success!');
     database = e.target.result
 
-if(navigator.onLine) {
-    console.log('Program back online')
-    checkDatabase;
+    if (navigator.onLine) {
+        console.log('Program back online')
+        checkDatabase;
 
-}
+    }
 }
 
 const saveRecord = (record) => {
     console.log('Record has been saved');
-    const purchase = db.purchase(['BudgetDB'], 'readrite');
-
-const storeData = purchase.objectStore('BudgetDB');
-
-storeData.add(record);
+    const transaction = database.transaction(['BudgetStore'], 'readwrite');
+    const storeData = transaction.objectStore('BudgetStore');
+    storeData.add(record);
 
 };
+
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase);
